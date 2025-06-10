92a2d7297e03a48ef1ade0e7571e9c177edb06f0
lucasbasep <lucas.base29@gmail.com>
2025-06-09 23:44:38 -0300

    feat(icons): add icons for projects

---
+++ src/assets/icons/programming/NextJSIcon.tsx
@@ -0,0 +1,15 @@
+import { cn } from "@/shared/utils/cn";
+import { SVGProps } from "react";
+
+export const NextJSIcon = ({className, ...rest}: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      viewBox="0 0 128 128"
+      xmlns="http://www.w3.org/2000/svg"
+      className={cn("w-6 h-6 ", className)}
+      {...rest}
+    >
+      <path d="M64 0A64 64 0 0 0 0 64a64 64 0 0 0 64 64 64 64 0 0 0 35.508-10.838L47.014 49.34v40.238H38.4V38.4h10.768l57.125 73.584A64 64 0 0 0 128 64 64 64 0 0 0 64 0m17.777 38.4h8.534v48.776L81.777 75.97Zm24.18 73.92-.111.096z"></path>
+    </svg>
+  );
+};
---
+++ src/types/IconTypeSVG.ts
@@ -0,0 +1,3 @@
+import { SVGProps } from "react";
+
+export type TIconSVGProps = SVGProps<SVGSVGElement>;
